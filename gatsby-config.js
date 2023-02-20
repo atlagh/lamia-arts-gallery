1d68800b40a9b7746e2ed6516e04b57a3e07a0cc
amine <atlagh.amine@gmail.com>
2023-02-20 17:36:13 +0100

    minor modifications

--- gatsby-config.js
+++ gatsby-config.js
@@ -5,9 +5,9 @@ module.exports = {
       options: {
         siteUrl: "https://gatsby-starter-portfolio-minimal-theme.netlify.app/", // Used for sitemap generation
         manifestSettings: {
-          favicon: "./content/images/favicon.png", // Path is relative to the root
-          siteName: "My Minimal Portfolio", // Used in manifest.json
-          shortName: "Portfolio", // Used in manifest.json
+          favicon: "./content/images/logo/logo3.png", // Path is relative to the root
+          siteName: "Lamia Arts Gallery", // Used in manifest.json
+          shortName: "Collection", // Used in manifest.json
           startUrl: "/", // Used in manifest.json
           backgroundColor: "#FFFFFF", // Used in manifest.json
           themeColor: "#000000", // Used in manifest.json
--- src/pages/index.js
+++ src/pages/index.js
@@ -13,7 +13,7 @@ import {
 export default function IndexPage() {
   return (
     <>
-      <Seo title="Gatsby Starter for Portfolio Minimal" />
+      <Seo title="Lamia Arts Gallery" />
       <Page useSplashScreenAnimation>
         <HeroSection sectionId="hero" />
         <ArticlesSection sectionId="articles" heading="Latest Articles" sources={['Medium']} />
